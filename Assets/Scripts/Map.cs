49e0fbb48abca9d2595f428134fe05893f8e2987
code-11 <brendan.ritter@yahoo.com>
2017-03-05 21:15:02 -0500

    Set up a simple grid.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	private Tile[,] tiles= new Tile[5,5];
+	public GameObject defaultObj;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < tiles.GetLength(0); x += 1) {
+			for (int y = 0; y < tiles.GetLength(1); y += 1) {
+				tiles [x, y] = new Tile (defaultObj, new Vector3 (x, y, 0));
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile{
+
+	GameObject container;
+
+	public Tile(GameObject type,Vector3 pos){
+		this.container=(GameObject)GameObject.Instantiate (type, pos,Quaternion.identity);
+	}
+}
